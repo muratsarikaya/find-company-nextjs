5007adb0a1b7e53c6c87ad03ea1c06da7a3318f8
Murat SARIKAYA <mrtsarkaya@gmail.com>
2022-03-09 23:47:56 +0300

    page preloader ekledi

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
-
+import Page from 'react-page-loading'
 
 const Layout = ({children}) => {
     return (
         <>
-            <Head>
-                <style
-                    data-href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
-                </style>
-            </Head>
-            <Header/>
-            <main style={{backgroundColor: "#fff", position: "relative", zIndex: 1}}>
-                {children}
-            </main>
-            <Footer/>
+            <Page loader={"rotate-spin"} color={"#6449e7"} size={5}>
+                <Head>
+                    <style
+                        data-href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
+                    </style>
+                </Head>
+                <Header/>
+                <main style={{backgroundColor: "#fff", position: "relative", zIndex: 1}}>
+                    {children}
+                </main>
+                <Footer/>
+            </Page>
         </>
     );
 };
--- components/layout/LayoutTwo.js
+++ components/layout/LayoutTwo.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
 import HeaderTwo from "../header/HeaderTwo";
 import Footer from "../footer/Footer";
-
+import Page from 'react-page-loading'
 
 const LayoutTwo = ({children}) => {
     return (
         <>
-            <Head>
-                <style
-                    data-href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
-                </style>
-            </Head>
-            <HeaderTwo/>
-            <main style={{backgroundColor: "#fff", position: "relative", zIndex: 1}}>
-                {children}
-            </main>
-            <Footer/>
+            <Page loader={"rotate-spin"} color={"#6449e7"} size={5}>
+                <Head>
+                    <style
+                        data-href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
+                    </style>
+                </Head>
+                <HeaderTwo/>
+                <main style={{backgroundColor: "#fff", position: "relative", zIndex: 1}}>
+                    {children}
+                </main>
+                <Footer/>
+            </Page>
         </>
     );
 };
