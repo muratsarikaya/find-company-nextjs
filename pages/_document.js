555278fc2a3881f00465090e7ab05ef2dee330af
Murat SARIKAYA <mrtsarkaya@gmail.com>
2022-06-18 02:12:20 +0300

    edited document

--- pages/_document.js
+++ pages/_document.js
@@ -1,43 +1,39 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
         const originalRenderPage = ctx.renderPage
 
-        // Run the React rendering logic synchronously
-        ctx.renderPage = () =>
-            originalRenderPage({
-                // Useful for wrapping the whole react tree
-                enhanceApp: (App) => App,
-                // Useful for wrapping in a per-page basis
-                enhanceComponent: (Component) => Component,
-            })
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
 
-        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-        const initialProps = await Document.getInitialProps(ctx)
-
-        return initialProps
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
     }
 
     render() {
         return (
             <Html lang="en">
-                <Head>
-                    <script async
-                        data-partytown-config
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                      partytown = {
-                                        lib: "/_next/static/~partytown/",
-                                        debug: true
-                                      };
-                                    `,
-                        }}
-                    />
-                </Head>
+                <Head />
                 <body>
-                <Main/>
-                <NextScript/>
+                <Main />
+                <NextScript />
                 </body>
             </Html>
         )
