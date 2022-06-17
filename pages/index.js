23f1517a0f2e8f3d71a4e6866eb71e008547683d
Murat SARIKAYA <mrtsarkaya@gmail.com>
2022-06-17 18:34:42 +0300

    optimization components

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,48 @@
-import Layout from "../components/layout/Layout";
 import 'react-dropdown/style.css';
-import TabMenuComp from "../components/advert/TabMenuComp";
-import FeaturedWrapper from "../components/featured/FeaturedWrapper";
-import RealEstateSection from "../components/realEstate/RealEstateSection";
-import WeSection from "../components/whyWe/WeSection";
-import RecentPropertySection from "../components/recentProperty/RecentPropertySection";
-import BlogSection from "../components/blog/BlogSection";
+import dynamic from "next/dynamic";
+import {Provider} from "react-redux";
+import store from "../Redux/store";
+//import Layout from "../components/layout/Layout";
+//import RecentPropertySection from "../components/recentProperty/RecentPropertySection"
+//import BlogSection from "../components/blog/BlogSection"
+//import RealEstateSection from "../components/realEstate/RealEstateSection"
+// import FeaturedWrapper from "../components/featured/FeaturedWrapper"
+// import WeSection from "../components/whyWe/WeSection"
+// import TabMenuComp from "../components/advert/TabMenuComp"
 
+const Layout = dynamic(
+    ()=> import("../components/layout/Layout"),
+    {ssr:true}
+)
+
+const RecentPropertySection = dynamic(
+    ()=> import("../components/recentProperty/RecentPropertySection"),
+    {ssr:true}
+    )
+const BlogSection = dynamic(()=> import("../components/blog/BlogSection"),
+    {ssr:true}
+);
+const RealEstateSection = dynamic(()=> import("../components/realEstate/RealEstateSection"),
+    {ssr:true}
+    ) ;
+const FeaturedWrapper = dynamic(()=> import("../components/featured/FeaturedWrapper"),
+    {ssr:true});
+const WeSection = dynamic(()=> import("../components/whyWe/WeSection"),
+    {ssr:true});
+const TabMenuComp = dynamic(()=> import("../components/advert/TabMenuComp"),
+    {ssr:true});
 
 export default function Home() {
     return (
-        <>
-            <Layout>
-                <TabMenuComp/>
-                <FeaturedWrapper/>
-                <RealEstateSection/>
-                <WeSection/>
-                <RecentPropertySection/>
-                <BlogSection/>
-            </Layout>
-        </>
+        <Provider store={store}>
+                <Layout>
+                    <TabMenuComp/>
+                    <FeaturedWrapper/>
+                    <RealEstateSection/>
+                    <WeSection/>
+                    <RecentPropertySection/>
+                    <BlogSection/>
+                </Layout>
+        </Provider>
     )
 }
