8a5a11e2e48772c207120ef6f5cf09928375bb18
Murat SARIKAYA <mrtsarkaya@gmail.com>
2022-06-18 01:37:31 +0300

    düzeltme

---
+++ Redux/store.js
@@ -0,0 +1,20 @@
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {hotelListReducer} from "./Reducers/HotelReducer";
+
+const reducer = combineReducers({
+    hotelList: hotelListReducer
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    //composeWithDevTools(applyMiddleware(...middleware))
+    applyMiddleware(...middleware)
+)
+
+export default store;
