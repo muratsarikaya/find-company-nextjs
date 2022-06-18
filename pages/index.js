55c31544d5653ee1be5984f89a07166620eea758
Murat SARIKAYA <mrtsarkaya@gmail.com>
2022-06-18 20:25:55 +0300

    edited index page

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,35 @@ import 'react-dropdown/style.css';
 import dynamic from "next/dynamic";
 import {Provider} from "react-redux";
 import store from "../Redux/store";
-import Layout from "../components/layout/Layout";
-//import RecentPropertySection from "../components/recentProperty/RecentPropertySection"
-import BlogSection from "../components/blog/BlogSection"
-import RealEstateSection from "../components/realEstate/RealEstateSection"
-import FeaturedWrapper from "../components/featured/FeaturedWrapper"
-import WeSection from "../components/whyWe/WeSection"
-import TabMenuComp from "../components/advert/TabMenuComp"
+// import Layout from "../components/layout/Layout";
+// //import RecentPropertySection from "../components/recentProperty/RecentPropertySection"
+// import BlogSection from "../components/blog/BlogSection"
+// import RealEstateSection from "../components/realEstate/RealEstateSection"
+// import FeaturedWrapper from "../components/featured/FeaturedWrapper"
+// import WeSection from "../components/whyWe/WeSection"
+// import TabMenuComp from "../components/advert/TabMenuComp"
 
-// const Layout = dynamic(
-//     ()=> import("../components/layout/Layout"),
+const Layout = dynamic(
+    ()=> import("../components/layout/Layout"),
+    {ssr:false}
+)
+
+// const RecentPropertySection = dynamic(
+//     ()=> import("../components/recentProperty/RecentPropertySection"),
 //     {ssr:true}
-// )
-//
-// // const RecentPropertySection = dynamic(
-// //     ()=> import("../components/recentProperty/RecentPropertySection"),
-// //     {ssr:true}
-// //     )
-// const BlogSection = dynamic(()=> import("../components/blog/BlogSection"),
-//     {ssr:true}
-// );
-// const RealEstateSection = dynamic(()=> import("../components/realEstate/RealEstateSection"),
-//     {ssr:true}
-//     ) ;
-// const FeaturedWrapper = dynamic(()=> import("../components/featured/FeaturedWrapper"),
-//     {ssr:true});
-// const WeSection = dynamic(()=> import("../components/whyWe/WeSection"),
-//     {ssr:true});
-// const TabMenuComp = dynamic(()=> import("../components/advert/TabMenuComp"),
-//     {ssr:true});
+//     )
+const BlogSection = dynamic(()=> import("../components/blog/BlogSection"),
+    {ssr:false}
+);
+const RealEstateSection = dynamic(()=> import("../components/realEstate/RealEstateSection"),
+    {ssr:false}
+    ) ;
+const FeaturedWrapper = dynamic(()=> import("../components/featured/FeaturedWrapper"),
+    {ssr:false});
+const WeSection = dynamic(()=> import("../components/whyWe/WeSection"),
+    {ssr:false});
+const TabMenuComp = dynamic(()=> import("../components/advert/TabMenuComp"),
+    {ssr:false});
 
 export default function Home() {
     return (
